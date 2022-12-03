76c6bd2985fc335dcea6a1ebfc26ef01439c4507
Bartek Kwapisz <kwapiszbartek@gmail.com>
2022-12-03 02:15:56 +0100

    navbar with useState
    now clicking adds and remove "line-through" class from the clicked button, but we also need to remove it from other buttons (do it tomorrow?)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,29 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Button(props) {
+  const buttonClasses = ["inline-block", "py-4", "text-xl", "text-white", "bg-gray-800", "px-7", "hover:bg-gray-700", "active:bg-gray-600", "rounded-xl", "m-1", "focus:ring", "focus:ring-orange-400"];
+  const [activeButton, setActiveButton] = useState(buttonClasses);
+  //try with isActive boolean
+  function activateButton(){
+    if(activeButton.includes("line-through")) {
+      setActiveButton(activeButton.filter(a => a !== "line-through"));
+    } else {
+      setActiveButton([
+        ...activeButton, "line-through"
+      ])
+    }
+  }
   return (
-    <button className="inline-block py-4 text-xl text-white bg-gray-800 px-7 hover:bg-gray-700 active:bg-gray-600 rounded-xl m-1 focus:ring focus:ring-orange-400 ">
-     
+    <button className={activeButton.join(" ")}
+     onClick={activateButton}>
       <span className="">
         {props.text}
       </span>
     </button>
   );
 }
-
+// https://stackoverflow.com/questions/70570025/only-one-active-button-in-react
 function Navbar() {
   return (
     <header className="p-4 bg-white shadow md:px-6 md:py-8 dark:bg-gray-900 flex items-center h-[80px] flex items-center justify-center">
@@ -18,11 +31,11 @@ function Navbar() {
           <span className="text-orange-400">Wb</span>.io
       </span></Link>
       <nav className="text-3xl font-bold text-center w-4/5">
-        <Link to='/offer-workers'><Button text="Offer workers"></Button></Link>
-        <Link to='/search-jobs'><Button text="Search jobs"></Button></Link>
-        <Link to='/search-workers'><Button text="Search workers"></Button></Link>
-        <Link to='/offer-jobs'><Button text="Offer jobs"></Button></Link>
-        <Link to='/account'><Button text="Account"></Button></Link>
+        <Link to='/offer-workers'><Button text="Offer workers" value="1"></Button></Link>
+        <Link to='/search-jobs'><Button text="Search jobs" value="2"></Button></Link>
+        <Link to='/search-workers'><Button text="Search workers" value="3"></Button></Link>
+        <Link to='/offer-jobs'><Button text="Offer jobs" value="4"></Button></Link>
+        <Link to='/account'><Button text="Account" value="5"></Button></Link>
       </nav>
     </header>
   );
